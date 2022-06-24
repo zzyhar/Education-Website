f8b771756cbbcf0c42595f3be8048a1ffe78b4b2
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-06-24 18:57:34 -0400

    FAQs Toggle using JavaScript

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 //change navbar styles on scroll
 
 window.addEventListener("scroll", () => {
-  documnet
+  document
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    //change icon
+    const icon = faq.querySelector(".faq-icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
 });
